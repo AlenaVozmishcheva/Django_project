a79da52499045b884f88b344ff45c3e8e2feb265
Alena Vozmishcheva <alena.glukhikh@bk.ru>
2025-04-13 23:12:42 +0300

    1. Написаны breeds_list_view и breed_dogs_list_view. 2. Написаны url's для breeds_list_view и breed_dogs_list_view. 3. Добавлены пустые шаблоны breeds.html и dogs.html.

--- dogs/views.py
+++ dogs/views.py
@@ -1,5 +1,3 @@
-from lib2to3.fixes.fix_input import context
-
 from django.shortcuts import render
 
 from dogs.models import Breed, Dog
@@ -11,4 +9,23 @@ def index_view(request):
         'title': 'Питомник - Главное'
     }
     return render(request, 'dogs/index.html', context=context)
+
+
+def breeds_list_view(request):
+    context = {
+        'objects_list' : Breed.objects.all(),
+        'title': 'Питомник - Все наши породы'
+    }
+    return render(request, 'dogs/breeds.html', context=context)
+
+def breed_dogs_list_view(request, pk: int):
+    breed_object = Breed.objects.get(pk=pk)
+    context = {
+        'objects_list' : Dog.objects.filter(breed_id=pk),
+        'title': f'Собаки породы - {breed_object.name}',
+        'breed_pk': breed_object.pk
+    }
+    return render(request, 'dogs/dogs.html', context=context)
+
+
 
