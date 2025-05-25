dc78c44c17c25013d3abb87525e8d222c27b06a8
Alena Vozmishcheva <Alena.Vozmishcheva@gmail.com>
2025-05-25 14:54:04 +0300

    Добавлен функционал валидирования пароля при регистрации.

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
 from users.models import User
+from users.validators import validate_password
+
 
 class StyleFormMixin:
     def __init__(self, *args, **kwargs):
@@ -26,7 +28,9 @@ class UserRegisterForm(StyleFormMixin, forms.ModelForm):
 
     def clean_password2(self):
         cleaned_data = self.cleaned_data
+        validate_password(cleaned_data['password'])
         if cleaned_data['password'] != cleaned_data['password2']:
+            print('Пароли не совпадают!')
             raise forms.ValidationError('Пароли не совпадают!!!')
         return cleaned_data['password2']
 
