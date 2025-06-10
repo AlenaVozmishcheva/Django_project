f73c5d3097a1d2d339f8ca7b2dce2c21f994a0cc
Alena Vozmishcheva <Alena.Vozmishcheva@gmail.com>
2025-06-11 02:25:36 +0300

    1. Добавлена карточка собаки. 2. Изменен шаблон вывода всех собак. 3. Обновлен шаблон обновления собаки 4. Исправлена ошибка в urls. 5. Изменен шаблон dogs/detail.html

--- dogs/views.py
+++ dogs/views.py
@@ -70,7 +70,7 @@ class DogDeactivatedListView(LoginRequiredMixin, ListView):
 class DogCreateView(LoginRequiredMixin, CreateView):
     model = Dog
     form_class = DogForm
-    template_name = 'dogs/create_update.html'
+    template_name = 'dogs/create.html'
     extra_context = {
         'title': 'Добавить собаку'
     }
@@ -97,7 +97,7 @@ class DogDetailView(DetailView):
 class DogUpdateView(LoginRequiredMixin, UpdateView):
     model = Dog
     form_class = DogForm
-    template_name = 'dogs/create_update.html'
+    template_name = 'dogs/update.html'
 
     def get_success_url(self):
         return reverse('dogs:dog_detail', args=[self.kwargs.get('pk')])
