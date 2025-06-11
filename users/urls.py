1138ac7ce4291738901c0248b7fea50d410653ab
Alena Vozmishcheva <Alena.Vozmishcheva@gmail.com>
2025-06-12 00:08:09 +0300

    Добавлен функционал просмотра всех пользователей.

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UserRegisterView, UserLoginView, UserProfileView, UserLogautView, UserUpdateVIEW, UserPasswordChangeView, user_generate_new_password_view
+from users.views import UserRegisterView, UserLoginView, UserProfileView, UserLogautView, UserUpdateVIEW, UserPasswordChangeView, user_generate_new_password_view, UserListView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
+        # Работа с аккаунтом
         path('', UserLoginView.as_view(), name='user_login'),
         path('logout/', UserLogautView.as_view(), name='user_logout'),
         path('register/', UserRegisterView.as_view(), name='user_register'),
@@ -13,4 +14,7 @@ urlpatterns = [
         path('update/', UserUpdateVIEW.as_view(), name='user_update'),
         path('change_password/', UserPasswordChangeView.as_view(), name='user_change_password'),
         path('profile/genpassword/', user_generate_new_password_view, name='user_generate_new_password'),
+
+        # Просмотр других пользователей
+        path('all_users/', UserListView.as_view(), name='users_list'),
 ]
